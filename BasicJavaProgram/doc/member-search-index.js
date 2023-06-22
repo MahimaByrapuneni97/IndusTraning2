7832471172c8665161d456ec4443d9e64c428ac9
MahimaByrapuneni97 <MahimaByrapuneni.97@gmail.com>
2023-06-22 16:50:47 -0400

    Addition of two numbers

---
+++ BasicJavaProgram/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.indus.training.basics","c":"Calc","l":"addtion(double, double)","u":"addtion(double,double)"},{"p":"com.indus.training.basics","c":"Calc","l":"Calc()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ BasicJavaProgram/src/main/java/com/indus/training/basics/Calc.java
@@ -0,0 +1,21 @@
+package com.indus.training.basics;
+/**
+ * Description: Basic java program. 
+ * @return
+ */
+public class Calc{
+	/**
+	 * Description:addition of two numbers.
+	 * @param parmeter1: All values must be post
+	 * @param parmeter2: all values must be postive
+	 * @return: return result value.
+	 */
+	public double addtion(double parmeter1, double parmeter2)
+	{
+	double result =0.0;	
+	result = parmeter1+parmeter2;
+	
+return result;
+
+}
+}
